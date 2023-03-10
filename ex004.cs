f02f01855e6659792b35fe9dab0cc4e6957f7d16
vhfsilva <30941086+VictoorFerreira@users.noreply.github.com>
2023-03-09 21:47:23 -0300

    Curso
    Operadores Relacionais

---
+++ ex004.cs
@@ -0,0 +1,26 @@
+using System;
+
+//namespace M02Ex004
+
+    class Program{
+        static void Main(String[] args){
+            int n1 = 0;
+            int n2 = 0;
+            //Entrada de dados
+            Console.Write("Digite um numero: ");
+            int.TryParse(Console.ReadLine(), out n1);
+            Console.Write("Digite outro numero: ");
+            int.TryParse(Console.ReadLine(), out n2);
+            //Saida dos Resultados
+            Console.WriteLine($"{n1} = {n2} ? {n1 == n2}");
+            Console.WriteLine($"{n1} != {n2} ? {n1 != n2}");
+            Console.WriteLine($"{n1} > {n2} ? {n1 > n2}");
+            Console.WriteLine($"{n1} < {n2} ? {n1 < n2}");
+            Console.WriteLine($"{n1} >= {n2} ? {n1 >= n2}");
+            Console.WriteLine($"{n1} <= {n2} ? {n1 <= n2}");
+            Console.WriteLine($"{n1} é inteiro ? {n1 is int}");
+            Console.WriteLine($"{n1} = {n2} ? {n1 == n2}");
+
+            Console.ReadKey();
+        }
+    }
